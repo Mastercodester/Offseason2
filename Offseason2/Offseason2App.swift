d50c89bb5f8e4a1b79134e1243fd14cb92692ddf
code <ajacks75@emich.edu>
2023-05-17 15:27:31 -0400

    Initial Commit

---
+++ Offseason2/Offseason2App.swift
@@ -0,0 +1,17 @@
+//
+//  Offseason2App.swift
+//  Offseason2
+//
+//  Created by Sean Carter on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Offseason2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
