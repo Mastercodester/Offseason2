17ccc997a8534d6b71e39f6f07de99e276a40426
Jacquese <jacqwhitson@gmail.com>
2023-05-19 11:23:42 -0400

    Test View Working properly with the map

--- Offseason2/Views/AddEvent.swift
+++ Offseason2/Views/AddEvent.swift
@@ -18,11 +18,20 @@ struct AddEvent: View {
     @State private var showPlaceLookupSheet = false
 
     @State var isPickerShowing = false
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
 
     @EnvironmentObject private var eventVm: EventViewModel
     @State var event: Event
     @Environment(\.dismiss) private var dismiss
     @FirestoreQuery(collectionPath: "events") var events : [Event]
+//    42.33157
+//    -83.045839
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 42.331578, longitude: -83.045839), span:MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    
+    @EnvironmentObject private var locationVm : LocationManager
+   
+//    (locationVm.location?.coordinate.latitude ?? 0.00),\(locationVm.location?.coordinate.longitude ?? 0.00)")
+
     var body: some View {   
         NavigationStack{
             VStack{
@@ -54,10 +63,20 @@ struct AddEvent: View {
                     
                     
                 }
-                List(events) { event in
-                    Text(event.name)
-                }  
-            }.toolbar{
+//                List(events) { event in
+//                    Text(event.name)
+//                }
+                Map(coordinateRegion: $mapRegion,showsUserLocation: true, annotationItems:events){
+                    annotation in
+                    MapMarker(coordinate: annotation.coordinate)
+                }.frame(height: 290)
+                
+                
+                 
+            }.onAppear {
+                mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: locationVm.location?.coordinate.longitude ?? 0.00, longitude: locationVm.location?.coordinate.latitude ?? 0.00), span:MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+            }
+            .toolbar{
                 ToolbarItem(placement: .automatic) {
                     saveButton
                    
@@ -83,6 +102,8 @@ struct AddEvent_Previews: PreviewProvider {
     static var previews: some View {
         AddEvent(event: Event())
         .environmentObject(EventViewModel())
+        .environmentObject(LocationManager())
+
         
     }
 }
